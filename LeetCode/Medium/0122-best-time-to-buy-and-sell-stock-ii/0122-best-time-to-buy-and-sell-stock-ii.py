90506fe5474bf847034495c1b51050b3e6c45122
tulikayaya <87579199+tulikayaya@users.noreply.github.com>
2026-03-09 17:30:24 -0400

    3 ms (54.89%) 20.4 MB (41.76%) 03-09-2026 0122-best-time-to-buy-and-sell-stock-ii Medium .py Array

---
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right, profit, maxP, Profit = 0, 1, 0, 0, 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit += prices[right] - prices[left]
+            left += 1
+            right +=1
+        return profit
+        
