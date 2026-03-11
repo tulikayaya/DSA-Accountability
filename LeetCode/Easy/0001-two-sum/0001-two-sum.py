e467d14aa7d2805aa634c8169cb354c3ea9cbee1
tulikayaya <87579199+tulikayaya@users.noreply.github.com>
2026-03-11 11:26:35 -0400

    2 ms (55.52%) 20.3 MB (65.54%) 03-11-2026 0001-two-sum Easy .py Array

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,17 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ind = {}
-        res = []
-        
+        ind, res = {}, []
         for i in range(len(nums)):
-            if (target - nums[i]) in ind:
-                res.append(ind[target - nums[i]])
+            if target - nums[i] in ind:
+                res.append(ind[target-nums[i]])
                 res.append(i)
-                break
-            else:
-                ind[nums[i]] = i
-        
+                return res
+            ind[nums[i]] = i
+
         return res
+
 
         
         
