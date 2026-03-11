88d6a5e3493f813281e9a05ee3aa1d82e18ec6d2
tulikayaya <87579199+tulikayaya@users.noreply.github.com>
2026-03-11 14:01:36 -0400

    67 ms (18.14%) 19.6 MB (76.2%) 03-11-2026 0680-valid-palindrome-ii Easy .py Two Pointers

---
+++ LeetCode/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def ispalindrome(left:int, right:int) -> bool:
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+            
+        left, right = 0, len(s)-1
+
+        while left <= right:
+            if s[left] != s[right]:
+                return ispalindrome(left+1, right) or ispalindrome(left, right-1)
+            left += 1
+            right -= 1
+        
+        return True
+
+        
